aa199ce85daded52439c34972f0aae1953760b86
anusha8380 <anusha.m@happiestminds.com>
2023-07-31 10:39:07 +0530

    added api's for fetch and a sample table along with navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useEffect, useState } from "react";
+import ResponsiveAppBar from './Components/Header';
+import BasicTable from './Components/Table'
+
+const App = () => {
+  const [users, setUsers] = useState([])
+
+  const fetchUserData = () => {
+    fetch("db.json")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data.empList)
+      })
+      
+  }
+
+  useEffect(() => {
+  fetchUserData()
+}, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ResponsiveAppBar/>
+      <BasicTable/>
+     {users.length > 0 && (
+      
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
+      
     </div>
   );
 }
